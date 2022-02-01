182119f0c5caf19e1d220994dfb1dbf6b3f3226f
Programisto1011 <victormaynougomez@gmail.com>
2022-02-01 15:03:51 +0000

    First version panel

--- src/js/view/Main.jsx
+++ src/js/view/Main.jsx
@@ -1,38 +1,54 @@
 import React from "react";
 
-//include images into your bundle
+//include album covers
 import rigoImage from "../../img/rigo-baby.jpg";
+import dog_cover from "../../img/dog_cover.jpg";
 //Instalar: npm install react-bootstrap bootstrap@5.1.3
 //import Button from 'react-bootstrap/Button';
-import ListGroup from "react-bootstrap/ListGroup";
-
+import { ListGroup, Badge } from "react-bootstrap";
 //create your first component
 const Main = () => {
 	return (
 		<>
-			<ListGroup>
-				<ListGroup.Item>No style</ListGroup.Item>
-				<ListGroup.Item variant="primary">Primary</ListGroup.Item>
-				<ListGroup.Item action variant="secondary">
-					Secondary
+			<ListGroup as="ol" numbered>
+				<ListGroup.Item
+					className="d-flex justify-content-between align-items-start"
+					action
+					variant="light">
+					<div className="ms-2 me-auto">
+						<div className="fw-bold">Subheading</div>
+						Cras justo odio
+					</div>
+					<Badge variant="primary" pill>
+						1
+					</Badge>
 				</ListGroup.Item>
-				<ListGroup.Item action variant="success">
-					Success
+				<ListGroup.Item
+					className="d-flex justify-content-between align-items-start"
+					action
+					variant="light">
+					<div className="ms-1 ">
+						<img src={dog_cover} alt="Dog cover" height="50px" />
+					</div>
+					<div className="ms-2 me-auto">
+						<div className="fw-bold">Subheading</div>
+						Cras justo odio
+					</div>
+					<Badge variant="primary" pill>
+						2
+					</Badge>
 				</ListGroup.Item>
-				<ListGroup.Item action variant="danger">
-					Danger
-				</ListGroup.Item>
-				<ListGroup.Item action variant="warning">
-					Warning
-				</ListGroup.Item>
-				<ListGroup.Item action variant="info">
-					Info
-				</ListGroup.Item>
-				<ListGroup.Item action variant="light">
-					Light
-				</ListGroup.Item>
-				<ListGroup.Item action variant="dark">
-					Dark
+				<ListGroup.Item
+					className="d-flex justify-content-between align-items-start"
+					action
+					variant="light">
+					<div className="ms-2 me-auto">
+						<div className="fw-bold">Subheading</div>
+						Cras justo odio
+					</div>
+					<Badge variant="primary" pill>
+						3
+					</Badge>
 				</ListGroup.Item>
 			</ListGroup>
 		</>
