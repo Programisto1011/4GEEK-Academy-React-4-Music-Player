865e066fac418da65b698240f843c7f0ba696497
Programisto1011 <victormaynougomez@gmail.com>
2022-02-14 16:37:19 +0000

    Last save

--- src/js/view/Main.jsx
+++ src/js/view/Main.jsx
@@ -1,67 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include album covers
-import dog_cover from "../../img/dog_cover.jpg";
-import cat_cover from "../../img/cat_cover.jpg";
-import monkey_cover from "../../img/monkey_cover.jpg";
+import dog_cover from "../../img/1.jpg";
+import cat_cover from "../../img/2.jpg";
+import monkey_cover from "../../img/3.jpg";
 //Instalar: npm install react-bootstrap bootstrap@5.1.3
 //import Button from 'react-bootstrap/Button';
 import { ListGroup, Badge } from "react-bootstrap";
 //create your first component
 const Main = () => {
+	const urlAPI = "https://assets.breatheco.de/apis/sound/";
+
+	const [state, setState] = useState([]);
+
+	const getSongsDetails = () => {
+		fetch(urlAPI.concat("songs"))
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error("fail to get people details");
+			})
+			.then((responseAddJSON) => {
+				console.log("Response add json", responseAddJSON);
+				setState(responseAddJSON);
+				console.log(`Store songs detail" ${state}`);
+			})
+			.catch((err) => {
+				console.error(err.message);
+			});
+	};
+
+	var actualSong = "files/mario/songs/castle.mp3";
+
+	useEffect(() => {
+		getSongsDetails();
+	}, []);
+	const changeSong = (actualSong = inputValue.url) => {};
+
+	const listSongs = state.map((inputValue, index) => (
+		<ListGroup.Item
+			key={index}
+			className="d-flex justify-content-between align-items-start"
+			action
+			onClick={() => (actualSong = inputValue.url)}
+			variant="light">
+			<div className="ms-1 ">
+				<img src={cat_cover} alt="Dog cover" height="50px" />
+			</div>
+			<div className="ms-2 me-auto">
+				<div className="fw-bold">{inputValue.name}</div>
+				{inputValue.category}
+			</div>
+			<Badge variant="primary" pill>
+				{inputValue.id}
+			</Badge>
+		</ListGroup.Item>
+	));
+
 	return (
 		<>
 			<ListGroup as="ol" numbered>
-				<ListGroup.Item
-					className="d-flex justify-content-between align-items-start"
-					action
-					variant="light">
-					<div className="ms-1 ">
-						<img src={cat_cover} alt="Dog cover" height="50px" />
-					</div>
-					<div className="ms-2 me-auto">
-						<div className="fw-bold">Subheading</div>
-						Cras justo odio
-					</div>
-					<Badge variant="primary" pill>
-						1
-					</Badge>
-				</ListGroup.Item>
-				<ListGroup.Item
-					className="d-flex justify-content-between align-items-start"
-					action
-					variant="light">
-					<div className="ms-1 ">
-						<img src={dog_cover} alt="Dog cover" height="50px" />
-					</div>
-					<div className="ms-2 me-auto">
-						<div className="fw-bold">Subheading</div>
-						Cras justo odio
-					</div>
-					<Badge variant="primary" pill>
-						2
-					</Badge>
-				</ListGroup.Item>
-				<ListGroup.Item
-					className="d-flex justify-content-between align-items-start"
-					action
-					variant="light">
-					<div className="ms-1 ">
-						<img
-							src={monkey_cover}
-							alt="Monkey cover"
-							height="50px"
-						/>
-					</div>
-					<div className="ms-2 me-auto">
-						<div className="fw-bold">Subheading</div>
-						Cras justo odio
-					</div>
-					<Badge variant="primary" pill>
-						3
-					</Badge>
-				</ListGroup.Item>
+				{listSongs}
 			</ListGroup>
+			<audio
+				controls
+				controlsList="nodownload"
+				autoPlay
+				className="container">
+				<source src={urlAPI + actualSong} type="audio/mpeg"></source>
+			</audio>
 		</>
 	);
 };
